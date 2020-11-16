2770d2b5769ac22d23b2c00387b0b1e825ae4709
Laura <laura.a.chelaru@gmail.com>
2020-11-16 16:43:16 +0100

    rimosso alert di saluto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-document.getElementById("metal").innerHTML = "Super Metal";
-alert("Ciao, Eli! Ciao, Enrico!");
+// document.getElementById("metal").innerHTML = "Super Metal";
+// alert("Ciao, Eli! Ciao, Enrico!");
